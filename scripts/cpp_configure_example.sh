244429a8dae849bd4f52cc787f15bd681a22864c
jilei-hao <jilei.cs@gmail.com>
2024-02-05 15:29:49 -0500

    added studygen

--- scripts/cpp_configure_example.sh
+++ scripts/cpp_configure_example.sh
@@ -3,4 +3,6 @@
 # This script is an example of how to configure a C++ project using CMake.
 # execute this script from the root of the project.
 cmake -S ./cpp -B ./build \
+  -DITK_DIR="/path/to/itk/binary" \
+  -DVTK_DIR="/path/to/vtk/binary" \
   -DCMAKE_BUILD_TYPE=Debug
